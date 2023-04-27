15e24df91148e487f1f1867c60d91c4e65affd05
Bruno <bvaleiro@gmail.com>
2023-04-27 12:08:37 -0300

    feature: add routes habits toggle and sumary

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -59,8 +59,71 @@ export async function appRoutes(app:FastifyInstance){
         completedHabits
       }
     })
-    app.get("/habits",async()=>{
-      const habits = await prisma.habits.findMany()
-      return habits
-    })    
+    app.patch('/habits/:id/toggle',async(request)=>{
+      const toggleHabitsParams = z.object({
+        id:z.string().uuid()
+      })
+      const {id} = toggleHabitsParams.parse(request.params)
+      const today = dayjs().startOf('day').toDate()
+      let day = await prisma.day.findUnique({
+        where:{
+          date:today
+        }
+      })
+      if(!day){
+        day= await prisma.day.create({
+          data:{
+            date:today
+          }
+        })
+      }
+      const dayHabit = await prisma.dayHabit.findUnique({
+        where:{
+          day_id_habit_id:{
+            day_id:day.id,
+            habit_id:id
+          }
+        }
+      })
+      if(dayHabit){
+        await prisma.dayHabit.delete({
+          where:{
+            id:dayHabit.id
+          }
+        })
+      }else{
+         await prisma.dayHabit.create({
+        data:{
+          day_id:day.id,
+          habit_id:id
+        }
+      })
+    }
+    })
+    app.get('/summary',async()=>{
+      const summary = await prisma.$queryRaw`
+        SELECT 
+          D.id, 
+          D.date,
+         (
+          SELECT 
+            cast(count(*) as float) 
+          from day_habits DH
+          where DH.day_id = D.id 
+         ) as completed,
+         (
+          SELECT
+            cast(count(*)as float)
+          from habit_week_days HWD
+          join habits H
+            on H.id = HWD.habit_id
+          where 
+            HWD.week_day = cast(strftime('%w',D.date/1000.0,'unixepoch')as int )
+            and H.created_at <= D.date
+         )as amount
+        FROM  days D
+
+      `
+      return summary
+    })
   }
