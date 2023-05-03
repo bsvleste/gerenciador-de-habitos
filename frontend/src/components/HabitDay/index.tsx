d1507c16059fcbefb40474144ab73ed1ffb21c9a
Bruno <bvaleiro@gmail.com>
2023-05-03 20:55:50 -0300

    feature: add component habi list

--- frontend/src/components/HabitDay/index.tsx
+++ frontend/src/components/HabitDay/index.tsx
@@ -1,9 +1,8 @@
 import * as Popover from '@radix-ui/react-popover';
-import * as Checkbox from '@radix-ui/react-checkbox';
-import { ProgressBar } from '../ProgressBar';
 import clsx from 'clsx'
-import { Check } from '@phosphor-icons/react';
 import dayjs from 'dayjs';
+import { ProgressBar } from '../ProgressBar';
+import { HabitList } from '../HabitList';
 interface HabitDayProps {
   completed?: number
   amount?: number
@@ -31,18 +30,7 @@ export function HabitDay({ completed = 0, amount = 0, date }: HabitDayProps) {
           <span className='font-semibold text-zinc-400'>{dayOfWeek}</span>
           <span className='mt-1 font-extrabold leading-tight text-3xl'>{dayAndMonth}</span>
           <ProgressBar progress={completedPercetange} />
-          <div className='mt-6 flex flex-col gap-3'>
-            <Checkbox.Root
-              className='flex items-center  gap-3 group'
-            >
-              <div className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-[1px] border-zinc-500 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500'>
-                <Checkbox.Indicator>
-                  <Check size={20} />
-                </Checkbox.Indicator>
-              </div>
-              <span className='font-semibold text-white text-xl leading-tight group-data-[state=checked]:line-through group-data-[state=checked]:text-zinc-400'>Beber 2L de agua</span>
-            </Checkbox.Root>
-          </div>
+          <HabitList date={date} />
           <Popover.Arrow height={8} width={16} className='fill-zinc-900' />
         </Popover.Content>
       </Popover.Portal>
---
+++ frontend/src/components/HabitList/index.tsx
@@ -0,0 +1,70 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import * as Checkbox from '@radix-ui/react-checkbox';
+import { Check } from '@phosphor-icons/react';
+import { useEffect, useState } from 'react';
+import { api } from '../../lib/axios';
+import dayjs from 'dayjs';
+interface HabiListProps {
+  date: Date
+}
+interface HabitsInfoProps {
+  possibleHabits: Array<{
+    id: string,
+    title: string,
+    created_at: string
+  }>
+  completedHabits: string[]
+}
+export function HabitList({ date }: HabiListProps) {
+  const [habistInfo, setHabitsInfo] = useState<HabitsInfoProps>()
+  const isDateInPast = dayjs(date).endOf('day').isBefore(new Date())
+  useEffect(() => {
+    api.get('/day', {
+      params: {
+        date: date.toISOString()
+      }
+    }).then(response => {
+      setHabitsInfo(response.data)
+    })
+  }, [])
+  async function handleToggleHabit(habitId: string) {
+    await api.patch(`/habits/${habitId}/toggle`)
+    const isHabitAlreadyCompleted = habistInfo!.completedHabits.includes(habitId)
+    let completedHabits: string[] = []
+    if (isHabitAlreadyCompleted) {
+      completedHabits = habistInfo!.completedHabits.filter(id => id !== habitId)
+
+    } else {
+      completedHabits = [...habistInfo!.completedHabits, habitId]
+    }
+    setHabitsInfo({
+      possibleHabits: habistInfo!.possibleHabits,
+      completedHabits
+    })
+  }
+  return (
+    <div className='mt-6 flex flex-col gap-3'>
+      {
+        habistInfo?.possibleHabits.map((habit) => (
+          <Checkbox.Root
+            onCheckedChange={() => handleToggleHabit(habit.id)}
+            checked={habistInfo.completedHabits.includes(habit.id)}
+            key={habit.id}
+            disabled={isDateInPast}
+            className='flex items-center  gap-3 group'>
+
+            <div className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-[1px] border-zinc-500 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500'>
+              <Checkbox.Indicator>
+                <Check size={20} />
+              </Checkbox.Indicator>
+            </div>
+            <span className='font-semibold text-white text-xl leading-tight group-data-[state=checked]:line-through group-data-[state=checked]:text-zinc-400'>
+              {habit.title}
+            </span>
+          </Checkbox.Root>
+
+        ))
+      }
+    </div>
+  )
+}
